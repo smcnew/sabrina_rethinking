4de663e8c09af059594eec188b27babd76efe958
smcnew <sabrina.mcnew@gmail.com>
2025-01-28 14:08:33 -0700

    Added more week 1 stuff

--- code/week_1.R
+++ code/week_1.R
@@ -1,8 +1,10 @@
 # Installing rethinking
-#
+# Make sure you update your R before you get going!
 
 install.packages(c("coda","mvtnorm","devtools"))
 library(devtools)
 devtools::install_github("rmcelreath/rethinking",ref="Experimental")
 
-install.packages("cmdstanr")
+# had to install this separately
+install.packages("cmdstanr", repos = c('https://stan-dev.r-universe.dev', getOption("repos")))
+
